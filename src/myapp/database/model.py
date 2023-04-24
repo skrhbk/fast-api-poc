e2c1395c2c9413617f33b63a9cc4cf93d6324f67
Clement Ke <skrhbk@gmail.com>
2023-04-25 02:31:39 +0800

    minor

--- src/myapp/database/model.py
+++ src/myapp/database/model.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+from pydantic import AnyUrl, BaseModel, EmailStr, Field, constr
 from pydantic import BaseModel
 from pydantic_mongo import ObjectIdField
 from bson import ObjectId
@@ -9,7 +10,7 @@ class User(BaseModel):
     id: ObjectIdField = None
     username: str
     hashed_password: str
-    email: Union[str, None] = None
+    email: Union[EmailStr, None] = None
     full_name: Union[str, None] = None
     disabled: Union[bool, None] = None
 
